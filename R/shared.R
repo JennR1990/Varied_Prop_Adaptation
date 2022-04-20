c42353c566d33762486afa9ae0d1117c853c8857
Jenn Ruttle <jennruttle@gmail.com>
2022-04-19 22:25:43 -0400

    Reran model code on cleaned data

--- R/shared.R
+++ R/shared.R
@@ -11,71 +11,20 @@ Loaddata<- function (group='passive', task='reaches') {
 }
  
 loadalldata<- function () {
-  pause_reaches<<- removeReachOutliers(Loaddata(group='pause'))
-  active_reaches<<- removeReachOutliers(Loaddata(group='active'))
-  passive_reaches<<- removeReachOutliers(Loaddata())
-  nocursor_reaches<<- removeReachOutliers(Loaddata(group='nocursor'))
-  nocursorI_reaches<<- removeReachOutliers(Loaddata(group='nocursor', task = 'NI_reaches'))
-  passive_localization<<- removeReachOutliers(Loaddata(task = 'localization'))
-  active_localization<<- removeReachOutliers(Loaddata(group='active', task = 'localization'))
-  nocursor_nocursors<<- removeReachOutliers(Loaddata(group='nocursor', task = 'nocursors'))
-  nocursorI_nocursors<<- removeReachOutliers(Loaddata(group='nocursor', task = 'NI_nocursors'))
-  nocursorI_reaches<<-nocursorI_reaches[,-9]
-  nocursorI_nocursors<<-nocursorI_nocursors[,-9]
-  newnocursor_reaches<<- cbind(nocursor_reaches, nocursorI_reaches[2:ncol(nocursorI_reaches)])
-  newnocursor_nocursors<<- cbind(nocursor_nocursors, nocursorI_nocursors[2:ncol(nocursorI_nocursors)])
-  terminal_reaches<<- removeReachOutliers(Loaddata(group='terminal'))
-  terminal_localization<<- removeReachOutliers(Loaddata(group='terminal', task = 'localization'))
-  pause_angles<<- Loaddata(group='Pause', task = "Angles")
-  active_angles<<- Loaddata(group='Active', task = "Angles")
-  passive_angles<<- Loaddata(group = "Pause", task = "Angles")
-  nocursor_angles<<- Loaddata(group='No-Cursor', task = "Angles")
   
-  Instructed<<- Loaddata(group = "Instructed_No-Cursors", task = "MovementTimes")
-  uninstructed<<- Loaddata(group="Uninstructed_No-Cursors", task = "MovementTimes")
-  
-  passive_prop_angles<<- Loaddata(group='Passive_Tap', task = "Angles")
-  active_prop_angles<<- Loaddata(group='Active_Tap', task = "Angles")
-  
-  no_cursorm<<-  removeReachOutliers(Loaddata(group = "no-cursor_maxvel", task = "uninstructed"))
-  no_cursormI<<- removeReachOutliers(Loaddata(group="no-cursor_maxvel", task = "instructed"))
-  no_cursormI<- no_cursormI[,-9]
+  variation_reaches<<- removeReachOutliers(Loaddata(group='Reaches', task = 'Baselined'))
+  variation_localizationsss<<- removeReachOutliers(Loaddata(group='Localizations', task = 'Baselined'))
+
   
 }
 
-fixnocursorcolnames<- function () {
-  
-  names<-colnames(newnocursor_reaches)
-  newnames<- c('p33','p34','p35','p36','p37','p38','p39','p40','p41','p42','p43','p44','p45','p46','p47')
-  names<- c(names[1:33], newnames)
-  colnames(newnocursor_reaches)<<- names
-  colnames(newnocursor_nocursors)<<- names
-}
+
 
 downloadOSFdata <- function(update=FALSE) {
   
   # this pulls data from the OSF repo:
-  files <- c('active_localization.csv'  = 'https://osf.io/mc523/?action=download',
-             'active_reaches.csv'       = 'https://osf.io/ejxy9/download',
-             'nocursor_nocursors.csv'   = 'https://osf.io/5b8s9/download',
-             'nocursor_reaches.csv'     = 'https://osf.io/vmnx7/download',
-             'nocursor_NI_nocursors.csv'   = 'https://osf.io/y4k2x/download',
-             'nocursor_NI_reaches.csv'     = 'https://osf.io/grnxh/download',
-             'passive_localization.csv' = 'https://osf.io/27v54/download',
-             'passive_reaches.csv'      = 'https://osf.io/mq5av/download',
-             'pause_reaches.csv'        = 'https://osf.io/q59b3/download',
-             'terminal_reaches.csv'     = 'https://osf.io/qdk9y/download',
-             'terminal_localization.csv'= 'https://osf.io/a9sx5/download',
-             'Active_Angles.csv'  = 'https://osf.io/ubdv8/?action=download',
-             'Passive_Angles.csv'       = 'https://osf.io/3nsqm/download',
-             'Pause_Angles.csv'   = 'https://osf.io/36cqd/download',
-             'No-Cursor_Angles.csv'     = 'https://osf.io/jyz2n/download',
-             'Instructed_No-Cursors_MovementTimes.csv'   = 'https://osf.io/8n3c6/download',
-             'Uninstructed_No-Cursors_MovementTimes.csv'     = 'https://osf.io/k4pze/download',
-             'Active_Tap_Angles.csv'  = 'https://osf.io/vkrs6/?action=download',
-             'Passive_Tap_Angles.csv'       = 'https://osf.io/f67m5/download',
-             'no-cursor_maxvel_instructed.csv'   = 'https://osf.io/62jbk/download',
-             'no-cursor_maxvel_uninstructed.csv'     = 'https://osf.io/zmcpf/download')
+  files <- c('Localizations_Baselined.csv'  = 'https://osf.io/zcyvq/?action=download',
+             'Reaches_Baselined.csv'       = 'https://osf.io/gjpm3/download',)
   
 
   # check if data directory exists and create if necessary:

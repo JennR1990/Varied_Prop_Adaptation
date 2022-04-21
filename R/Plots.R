d2b06e3567ee56a9503711e8a061b335a47ba668
Jenn Ruttle <jennruttle@gmail.com>
2022-04-21 18:12:18 -0400

    remade variability plot with confidence intervals

--- R/Plots.R
+++ R/Plots.R
@@ -1750,3 +1750,144 @@ plotDecaymodels<- function() {
 }
 
 
+##Plotting learning rates and asymptotes after bootstrapping across blocks
+plotLR_Aperblock<- function(){
+  phase0<- c(3,6,10,20)
+  remove<- c(phase0, (phase0+24))
+  df<- read.csv("ana/asymptoticDecayParameterCIs.csv", header = TRUE)
+  
+  
+  newdf<- df[-remove,]
+  
+  
+  phases<- as.character(c(1,2,4,5,7,8,9,11,12,13:19,21:24))
+  
+  trialsets <- list('1'=c(50:61),      "2" = c(62:73),      "3" = c(74:85),   "4" = c(86:97), 
+                    "5" = c(98:109),   "6" = c(110:133),    "7" = c(134:157), "8" = c(158:181),
+                    "9" = c(194:205), "10" = c(206:217),   "11" = c(218:229), "12" = c(254:265),
+                    "13" = c(266:289),   "14" = c(290:313),   "15" = c(314:325),   "16" = c(326:337),
+                    "17" = c(350:361),   "18" = c(362:373),   "19" = c(374:385),   "20" = c(386:397),
+                    "21" = c(410:421),   "22" = c(422:433),   "23" = c(434:445),   "24" = c(446:457))
+  
+  
+  
+  rot<- c()
+  for (phase in phases) rot<-c(rot,as.numeric(unique(variation_reaches[as.numeric(unlist(trialsets[phase])),1])))
+  
+  rotation<- c(rot,rot)
+  newdf<- cbind(newdf,rotation)
+  
+  newN0<- (newdf$N0/abs(newdf$rotation))*100
+  newN_97<- (newdf$N0_975/abs(newdf$rotation))*100
+  newN_25<- (newdf$N0_025/abs(newdf$rotation))*100
+  newN_5<- (newdf$N0_5/abs(newdf$rotation))*100
+  
+  svglite("figs/LR & Asymptotes Across Blocks Together.svg", height = 5, width = 14)
+  layout(matrix(1:2, nrow = 1, byrow = TRUE), heights = c(2))
+  
+  plot(newN_5[1:20], type = "l", xlim = c(0,20),ylim = c(0,200), col = "Red", main = "Asymptotes Across Blocks", ylab = "Percent Compensation", xlab = "Block (12 or 24 trials)", axes = FALSE)
+  indx<- seq(from = 0, to = 20, by = 4)
+  indx[1]<- 1
+  axis(1, at = indx)
+  axis(2, at = c(0,25,50,75,100,125,150,175,200), las = 2)
+  locCI<-c(newN_25[1:20],rev(newN_97[1:20]))
+  x<- c(1:20,20:1)
+  polygon(x, locCI, col = rgb(1,0,0,.2), border = NA)
+  
+  
+  #plot(newN_5[21:40], type = "l", ylim = c(0,200), col = "Blue", main = "Reach Asymptotes Across Blocks", ylab = "Percent Compensation", xlab = "Block (12 or 24 trials)", axes = FALSE)
+  lines(newN_5[21:40], col = "Blue")
+  #axis(1, at = indx)
+  #axis(2, at = c(0,25,50,75,100,125,150,175,200), las = 2)
+  reachCI<-c(newN_25[21:40],rev(newN_97[21:40]))
+  polygon(x, reachCI, col = rgb(0,0,1,.2), border = NA)
+  
+  legend(3,200, legend= c("Localizations", "Reaches"), col = c("Red", "Blue"), lty = 1, lwd = 1, bty = "n")
+  
+  
+  plot(df$lambda_500[1:20], type = "l", ylim = c(0,1), col = "Red", main = "Learning Rates Across Blocks", ylab = "Amount learned per trial", xlab = "Block (12 or 24 trials)", axes = FALSE)
+  
+  locCI<-c(df$lambda_025[1:20],rev(df$lambda_975[1:20]))
+  x<- c(1:20,20:1)
+  axis(1, at = indx)
+  axis(2, at = c(0,.25,.5,.75,1), las = 2)
+  polygon(x, locCI, col = rgb(1,0,0,.2), border = NA)
+  
+  
+  #plot(df$lambda_500[21:40], type = "l", ylim = c(0,1), col = "Blue", main = "Reach Learning Rates Across Blocks", ylab = "Amount learned per trial", xlab = "Block (12 or 24 trials)", axes = FALSE)
+  lines(df$lambda_500[21:40], col = "Blue")
+  axis(1, at = indx)
+  axis(2, at = c(0,.25,.5,.75,1), las = 2)
+  reachCI<-c(df$lambda_025[21:40],rev(df$lambda_975[21:40]))
+  polygon(x, reachCI, col = rgb(0,0,1,.2), border = NA)
+  legend(3,1, legend= c("Localizations", "Reaches"), col = c("Red", "Blue"), lty = 1, lwd = 1, bty = "n")
+  dev.off()
+}
+
+##looking at variability across blocks
+
+variabilityplot<-function() {
+variation_localization<- read.csv("data/variation_localization.csv", header = TRUE)
+variation_reaches<- read.csv("data/variation_reaches.csv", header = TRUE) 
+
+trialsets <- list('1'=c(50:61),      "2" = c(62:73),   "4" = c(86:97), 
+                  "5" = c(98:109),    "7" = c(134:157), "8" = c(158:181),
+                  "9" = c(194:205),   "11" = c(218:229), "12" = c(254:265),
+                  "13" = c(266:289),   "14" = c(290:313),   "15" = c(314:325),   "16" = c(326:337),
+                  "17" = c(350:361),   "18" = c(362:373),   "19" = c(374:385),
+                  "21" = c(410:421),   "22" = c(422:433),   "23" = c(434:445),   "24" = c(446:457))
+
+
+
+sdCIsr<- data.frame()
+sdCIsl<- data.frame()
+
+participants<- 2:33
+
+for (participant in participants){
+for (trial in 1:20){
+  
+stop<-as.numeric(max(trialsets[[trial]]))
+start<- stop - 3
+
+sdCIsr[trial,participant-1]<-sd(variation_reaches[start:stop,participant], na.rm = TRUE)
+sdCIsl[trial,participant-1]<-sd(variation_localization[start:stop,participant], na.rm = TRUE)
+
+}
+}
+
+datar<- cbind(1:2, sdCIsr)
+plot(rowMeans(datar[,2:33], na.rm = TRUE), type = "l", col = "Blue", ylim = c(0,12), axes = FALSE, xlab = "Block of Trials (12 or 24)", ylab = "Standard Deviation [°]", main = "Variability Over Blocks")
+indx<- seq(from = 0, to = 20, by = 4)
+indx[1]<- 1
+axis(1, at = indx)
+axis(2, at = c(0,2,4,6,8,10,12), las = 2)
+dataCIs<- trialCI(datar)
+y <- c(dataCIs[, 1], rev(dataCIs[, 2]))
+x = c(c(1:20), rev(c(1:20)))
+polygon(x,y,col = rgb(0,0,1,.2), border = NA )
+legend(3,3, legend= c("Localizations, r2 = .01", "Reaches, r2 = .36*"), col = c("Red", "Blue"), lty = 1, lwd = 1, bty = "n")
+reaches<-rowMeans(datar[,2:33], na.rm = TRUE)
+time<- 1:20
+stuff<- data.frame(reaches, time)
+abline(lm(reaches~time, data = stuff), col = "blue")
+model<-lm(reaches~time, data = stuff)
+
+
+data<- cbind(1:2, sdCIsl)
+lines(rowMeans(data[,2:33], na.rm = TRUE), col = "red")
+dataCIs<- trialCI(data)
+y <- c(dataCIs[, 1], rev(dataCIs[, 2]))
+x = c(c(1:20), rev(c(1:20)))
+polygon(x,y,col = rgb(1,0,0,.2), border = NA )
+localizations<-rowMeans(data[,2:33], na.rm = TRUE)
+time<- 1:20
+stuff<- data.frame(localizations, time)
+abline(lm(localizations~time, data = stuff), col = "red")
+
+model<-lm(localizations~time, data = stuff)
+summary(model)
+
+
+
+}
